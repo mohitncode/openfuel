579887483ff453b2ca034153078c2a73bd980938
Mohit Delhiwala <mohithem@buffalo.edu>
2018-01-01 01:07:14 -0500

    Removed unused folders. Cleaned up unneeded references

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <title>Openfuel</title>
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/normalize.css" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/foundation.css" />
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/home_view.css" />  
+  <link rel="stylesheet" type="text/css" href="./assets/css/normalize.css" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/foundation.css" />
+  <link rel="stylesheet" type="text/css" href="./assets/css/home_view.css" />
   <!--[if lte IE 8]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
 </head>
 
@@ -21,15 +21,15 @@
   </div>
 
   <div id="nav">
-  <?php 
-   
+  <?php
+
     $this->load->view('navigation');
-  
+
   ?>
   </div>
 
   <div class="row" id="login_splash">
-    <form action="<?php echo base_url(); ?>login" method="post" id="login" name="login">
+    <form action="./login" method="post" id="login" name="login">
       <div class="row">
         <div class="large-6 columns small-centered padded-form">
           <label for="email_id">Email ID</label>
@@ -71,17 +71,17 @@
 
     <ul class="ch-grid">
           <li>
-            <div class="ch-item ch-img-1">        
+            <div class="ch-item ch-img-1">
               <div class="ch-info-wrap">
                 <div class="ch-info">
                   <div class="ch-info-front ch-img-1"></div>
                   <div class="ch-info-back">
-                    <p>To showcase your projects & ideas to the world, sign up <a href="<?php echo base_url();?>register/user_registration">HERE</a></p>
-                  </div>  
+                    <p>To showcase your projects & ideas to the world, sign up <a href="./register/user_registration">HERE</a></p>
+                  </div>
                 </div>
               </div>
             </div>
-            <br><img src="<?php echo base_url(); ?>/assets/img/ajax-loader.gif">
+            <br><img src=".//assets/img/ajax-loader.gif">
           </li>
           <li>
             <div class="ch-item ch-img-2">
@@ -89,12 +89,12 @@
                 <div class="ch-info">
                   <div class="ch-info-front ch-img-2"></div>
                   <div class="ch-info-back">
-                    <p>To let the world know about your event(s), to make it's news spread like fire, sign up <a href="<?php echo base_url();?>register/org_registration">HERE</a></p>
+                    <p>To let the world know about your event(s), to make it's news spread like fire, sign up <a href="./register/org_registration">HERE</a></p>
                   </div>
                 </div>
               </div>
             </div>
-            <br><img src="<?php echo base_url(); ?>/assets/img/ajax-loader.gif">
+            <br><img src=".//assets/img/ajax-loader.gif">
           </li>
           <li>
             <div class="ch-item ch-img-3">
@@ -102,17 +102,17 @@
                 <div class="ch-info">
                   <div class="ch-info-front ch-img-3"></div>
                   <div class="ch-info-back">
-                    <p>To help budding enterpreneurs find direction with their start up, sign up <a href="<?php echo base_url();?>register/mentor_registration">HERE</a></p>
+                    <p>To help budding enterpreneurs find direction with their start up, sign up <a href="./register/mentor_registration">HERE</a></p>
                   </div>
                 </div>
               </div>
             </div>
-            <br><img src="<?php echo base_url(); ?>/assets/img/ajax-loader.gif">
+            <br><img src=".//assets/img/ajax-loader.gif">
           </li>
         </ul>
 
    </div>
-   
+
   </div>
 
   <?php $this->load->view('includes/footer'); ?>
@@ -127,7 +127,7 @@
     ?>
  </div>
 
-  <script type="text/javascript" src="http://localhost/openfuel/assets/js/shapeshifter.js"></script>
+  <script type="text/javascript" src="./assets/js/shapeshifter.js"></script>
   <?php $this->load->view('includes/jquery'); ?>
 
   <script type="text/javascript">
@@ -145,4 +145,4 @@
 
   </script>
 </body>
-</html> 
+</html>
--- application/views/navigation.php
+++ application/views/navigation.php
@@ -2,20 +2,20 @@
     <ul class="title-area">
 		<!-- Title Area -->
 		<li class="name">
-			<h1><a href="<?php echo base_url(); ?>events/"><img src="<?php echo base_url(); ?>assets/img/logo.png" alt="Openfuel" height="30px" width="180px"></a></h1>
+			<h1><a href="./"><img src="./assets/img/logo.png" alt="Openfuel" height="30px" width="180px"></a></h1>
 		</li>
 		<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
 	</ul>
-	
+
     <section class="top-bar-section">
 		<!-- Right Nav Section -->
 		<ul class="right">
-			
-			<?php 
-				
+
+			<?php
+
 				if($this->session->userdata('user_type') == 'organization')
 				{
-					
+
 				?>
 				<!-- <li id="sb-search" class="sb-search">
 					<form>
@@ -26,7 +26,7 @@
 				</li> -->
 				<li class="divider"></li>
 				<li class="has-dropdown">
-					<a href="<?php echo base_url(); ?>events/">Events</a>
+					<a href="./events/">Events</a>
 					<ul class="dropdown">
 						<li><label>Filter Events</label></li>
 						<li><a href="#">This Week</a></li>
@@ -37,26 +37,26 @@
 				</li>
 				<li class="divider"></li>
 				<li class="has-dropdown">
-					<a href="<?php echo base_url(); ?>events/events_info">Organize</a>
+					<a href="./events/events_info">Organize</a>
 					<ul class="dropdown">
-						<li><a href="<?php echo base_url(); ?>events/create_event">Event</a></li>
+						<li><a href="./events/create_event">Event</a></li>
 					</ul>
 					<li class="divider"></li>
 				</li>
 				<li class="has-dropdown">
-					<a href="<?php echo base_url(); ?>profile">Profile</a>
+					<a href="./profile">Profile</a>
 					<ul class="dropdown">
-						<li><a href="<?php echo base_url(); ?>profile/edit_profile">Edit Profile</a></li>
-						<li><a href="<?php echo base_url(); ?>login/logout">Logout</a></li>
+						<li><a href="./profile/edit_profile">Edit Profile</a></li>
+						<li><a href="./login/logout">Logout</a></li>
 					</ul>
 				</li>
-				
+
 				<?php
-					
+
 				}
 				elseif($this->session->userdata('user_type') == 'user')
 				{
-					
+
 				?>
 				<!-- <li id="sb-search" class="sb-search">
 					<form>
@@ -65,10 +65,10 @@
 					<span class="sb-icon-search"></span>
 					</form>
 				</li> -->
-				
+
 				<li class="divider"></li>
 				<li class="has-dropdown">
-					<a href="<?php echo base_url(); ?>events/">Events</a>
+					<a href="./events/">Events</a>
 					<ul class="dropdown">
 						<li><label>Filter Events</label></li>
 						<li><a href="#">This Week</a></li>
@@ -79,28 +79,28 @@
 				</li>
 				<li class="divider"></li>
 				<li class="has-dropdown">
-					<a href="<?php echo base_url(); ?>projects/my_projects">Projects</a>
+					<a href="./projects/my_projects">Projects</a>
 					<ul class="dropdown">
-						<li><a href="<?php echo base_url(); ?>projects/create_project">Create Project</a></li>
+						<li><a href="./projects/create_project">Create Project</a></li>
 						<li class="divider"></li>
-						<li><a href="<?php echo base_url(); ?>projects/my_projects">View my projects &nbsp &rarr;</a></li>
+						<li><a href="./projects/my_projects">View my projects &nbsp &rarr;</a></li>
 					</ul>
 				</li>
 				<li class="divider"></li>
 				<li class="has-dropdown">
-					<a href="<?php echo base_url(); ?>profile">Profile</a>
+					<a href="./profile">Profile</a>
 					<ul class="dropdown">
-						<li><a href="<?php echo base_url(); ?>profile/edit_profile">Edit Profile</a></li>
-						<li><a href="<?php echo base_url(); ?>login/logout">Logout</a></li>
+						<li><a href="./profile/edit_profile">Edit Profile</a></li>
+						<li><a href="./login/logout">Logout</a></li>
 					</ul>
 				</li>
-				
+
 				<?php
-					
+
 				}
 				elseif($this->session->userdata('user_type') == 'mentor')
 				{
-					
+
 				?>
 				<!-- <li id="sb-search" class="sb-search">
 					<form>
@@ -111,7 +111,7 @@
 				</li> -->
 				<li class="divider"></li>
 				<li class="has-dropdown">
-					<a href="<?php echo base_url(); ?>events/">Events</a>
+					<a href="./events/">Events</a>
 					<ul class="dropdown">
 						<li><label>Filter Events</label></li>
 						<li><a href="#">This Week</a></li>
@@ -129,24 +129,18 @@
 				</li>
 				<li class="divider"></li>
 				<li class="has-dropdown">
-					<a href="<?php echo base_url(); ?>profile">Profile</a>
+					<a href="./profile">Profile</a>
 					<ul class="dropdown">
-						<li><a href="<?php echo base_url(); ?>profile/edit_profile">Edit Profile</a></li>
-						<li><a href="<?php echo base_url(); ?>login/logout">Logout</a></li>
+						<li><a href="./profile/edit_profile">Edit Profile</a></li>
+						<li><a href="./login/logout">Logout</a></li>
 					</ul>
 				</li>
-				
+
 				<?php
-					
+
 				}
-				
-				else
-				{
-					
-				}
-				
 			?>
-			
+
 		</ul>
 	</section>
 </nav>
